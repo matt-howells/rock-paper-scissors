4a8d3264912369a0bea282454c23ff438af0af7b
matt-howells <matthowells@icloud.com>
2024-09-25 22:03:45 -0700

    create computer choice function

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
-console.log ("poop")
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3 + 1);
+}
+
+let choice = getComputerChoice()
+
+if (choice === 1 ) {
+    console.log('rock')
+} else if (choice === 2 ) {
+    console.log('paper') 
+} else {
+    console.log('scissors')
+}
